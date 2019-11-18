aa253450ad809513afdcf36b2bff8c56377ab147
MagnavoxTG <dominik.lohner92@gmx.at>
2019-11-18 11:43:51 +0100

    Release 1.0

---
+++ cmake-build-release/CMakeFiles/XChiffText.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/XChiffText.dir/main.c.o"
+  "XChiffText"
+  "XChiffText.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/XChiffText.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
